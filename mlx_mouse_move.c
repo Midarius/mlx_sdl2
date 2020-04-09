c2f531332010419d8b6102437cfb7dd416678c96
Midarius <26384743+Midarius@users.noreply.github.com>
2020-04-09 15:51:00 +0200

    updated mlx_mouse_move

--- mlx_mouse_move.c
+++ mlx_mouse_move.c
@@ -6,14 +6,16 @@
 /*   By: fcals <fcals@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/15 17:37:41 by fcals             #+#    #+#             */
-/*   Updated: 2020/03/15 17:37:43 by fcals            ###   ########.fr       */
+/*   Updated: 2020/04/09 15:47:28 by fcals            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <SDL.h>
+#include <mlx_private.h>
+
 int	mlx_mouse_move(void *win_ptr, int x, int y)
 {
-	(void)win_ptr;
-	(void)x;
-	(void)y;
+	if (win_ptr && x >= 0 && y >= 0)
+		SDL_WarpMouseInWindow(((t_mlx_window*)win_ptr)->window, x, y);
 	return (0);
 }
